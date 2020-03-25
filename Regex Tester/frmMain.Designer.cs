494a444051c9df4db864cdfc28c6903ce5fc0973
Ray-Arthur <27481892+StarZHF@users.noreply.github.com>
2020-03-25 01:09:27 -0500

    Add files via upload

---
+++ Regex Tester/frmMain.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Regex_Tester
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.tbOutput = new System.Windows.Forms.TextBox();
+            this.tbExpression = new System.Windows.Forms.TextBox();
+            this.btnParse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbInput
+            // 
+            this.tbInput.Location = new System.Drawing.Point(13, 13);
+            this.tbInput.Multiline = true;
+            this.tbInput.Name = "tbInput";
+            this.tbInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbInput.Size = new System.Drawing.Size(775, 236);
+            this.tbInput.TabIndex = 0;
+            // 
+            // tbOutput
+            // 
+            this.tbOutput.Location = new System.Drawing.Point(13, 281);
+            this.tbOutput.Multiline = true;
+            this.tbOutput.Name = "tbOutput";
+            this.tbOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbOutput.Size = new System.Drawing.Size(775, 157);
+            this.tbOutput.TabIndex = 0;
+            // 
+            // tbExpression
+            // 
+            this.tbExpression.Location = new System.Drawing.Point(13, 255);
+            this.tbExpression.Name = "tbExpression";
+            this.tbExpression.Size = new System.Drawing.Size(694, 20);
+            this.tbExpression.TabIndex = 1;
+            // 
+            // btnParse
+            // 
+            this.btnParse.Location = new System.Drawing.Point(713, 253);
+            this.btnParse.Name = "btnParse";
+            this.btnParse.Size = new System.Drawing.Size(75, 23);
+            this.btnParse.TabIndex = 2;
+            this.btnParse.Text = "Parse";
+            this.btnParse.UseVisualStyleBackColor = true;
+            this.btnParse.Click += new System.EventHandler(this.btnParse_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnParse);
+            this.Controls.Add(this.tbExpression);
+            this.Controls.Add(this.tbOutput);
+            this.Controls.Add(this.tbInput);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.Text = "Regex Helper | UID: 69588";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.TextBox tbOutput;
+        private System.Windows.Forms.TextBox tbExpression;
+        private System.Windows.Forms.Button btnParse;
+    }
+}
+
---
+++ Regex Tester/frmMain.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Regex_Tester
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnParse_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                tbOutput.Text = null;
+                foreach(Match m in Regex.Matches(tbInput.Text, tbExpression.Text))
+                {
+                    tbOutput.AppendText(m.Value + "\r\n");
+                }
+            }
+            catch (Exception ex) // In the event of a severe fuck-up.
+            {
+                tbOutput.Text = ex.Message;
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            MaximumSize = Size;
+            MinimumSize = Size;
+        }
+    }
+}
